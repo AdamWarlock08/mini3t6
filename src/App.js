48011246ad7ce912433b8a471a8a1f8d68edf517
Stve Erickson <ice000@msn.com>
2020-06-18 02:30:55 -0600

    add task

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+\  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:'',
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+  this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""{
+      const items=[...this.state.items, newItems];
+    })
+  }
   render(){
     return(
      <div className="App">
         <header>
-          <form id="to-do-form">
-            <input type="text" placeholder="Enter a task"/>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter a task" value={this.state.currentItem.text} onChange={this.handleInput}/>
             <button type="submit">Add</button>
           </form>
       </header>
